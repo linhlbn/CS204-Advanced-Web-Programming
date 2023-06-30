3c9dbdfc8054d5e3e13f839453e78516f21d0fdb
apricity <49004582+linhlbn@users.noreply.github.com>
2023-06-30 13:09:11 +0700

    update UI
    landing page, dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,28 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 flex justify-center">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <!-- anything after login at user page -->
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg text-center">
+                <!-- Welcome message after login at user page -->
+                <div class="p-6 text-gray-900" style="border-bottom:1px solid #ddd;">
+                    {{ __("Welcome to Student's Page. Our latest study info will update soon!") }}
+                </div>
+                <!-- Latest Information div -->
+                <div class="p-6 text-gray-900 bg-gray-100 mt-4" style="border-radius:10px;">
+                    <h3 class="font-semibold text-lg text-gray-800 leading-tight mb-2" style="text-decoration: underline;">Latest Information</h3>
+                    <!-- You can update the latest information here -->
+                    <p style="text-align:center;">  </p>
+                </div>
+                <!-- Your Messages div -->
+                <div class="p-6 text-gray-900 bg-gray-100 mt-4" style="border-radius:10px;">
+                    <h3 class="font-semibold text-lg text-gray-800 leading-tight mb-2" style="text-decoration: underline;">Your Messages</h3>
+                    <!-- You can update the user messages here -->
+                    <p style="text-align:center;">  </p>
                 </div>
             </div>
         </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,22 +1,9 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
+    <div class="absolute top-0 right-0 flex justify-between h-16">
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex" style="background-color: green;">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div>
+            
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
